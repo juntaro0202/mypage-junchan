7074460153efe9243d688e03acc576faaed73f81
juntaro0202 <zyunn14641@gmail.com>
2020-09-12 23:42:19 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<template>
+  <v-app>
+    <v-navigation-drawer app v-model="drawer" clipped>
+      <v-container>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title class="title grey--text text--darken-2">
+              Navigation lists
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-divider></v-divider>
+        <v-list dense nav>
+          <v-list-item v-for="nav_list in nav_lists" :key="nav_list.name">
+            <v-list-item-icon>
+              <v-icon>{{ nav_list.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>{{ nav_list.name }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-container>
+    </v-navigation-drawer>
+    <v-app-bar color="primary" dark app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Vuetify</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn text>For Enterprise</v-btn>
+        <v-menu offset-y>
+          <template v-slot:activator="{ on }">
+            <v-btn v-on="on" text>Support</v-btn>
+          </template>
+          <v-list>
+            <v-subheader>Get help</v-subheader>
+            <v-list-item v-for="support in supports" :key="support">
+              <v-list-item-content>
+                <v-list-item-title>{{ support }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </v-toolbar-items>
+    </v-app-bar>
+    <v-main>
+      <router-view />
+    </v-main>
+    <v-footer color="primary" dark app>
+      Vuetify
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: null,
+      supports: [
+        "Consulting and suppourt",
+        "Discord community",
+        "Report a bug",
+        "Github issue board",
+        "Stack overview",
+      ],
+
+      nav_lists: [
+        { name: "Getting Started", icon: "mdi-vuetify" },
+        { name: "Customization", icon: "mdi-cogs" },
+        { name: "Styles & animations", icon: "mdi-palette" },
+        { name: "UI Components", icon: "mdi-view-dashboard" },
+        { name: "Directives", icon: "mdi-function" },
+        { name: "Preminum themes", icon: "mdi-vuetify" },
+      ],
+    };
+  },
+};
+</script>
